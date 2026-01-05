bb50d161defb3d298b7d75a74bf94bc0c64ecdf6
Milton <ajaxmilton@hotmail.com>
2026-01-05 18:19:37 +0200

    add basic todo app structure with input and add button

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [task, setTask] = useState();
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleClick = () => {
+    console.log(task);
+  };
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Todo App</h1>
+      <div className="container">
+        <input
+          type="text"
+          onChange={(e) => handleChange(e)}
+          value={task}
+          className="input-field"
+        />
+        <button className="btn" onClick={handleClick}>
+          Add
+        </button>
+      </div>
     </>
   );
 }
